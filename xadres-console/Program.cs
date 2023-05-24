ccfacd52ce00c08a17f2b1211da0c574b2232068
Breno Maciel <maciel_lpnm@hotmail.com>
2023-05-24 15:38:25 -0300

    Criando classe posição

--- xadres-console/Program.cs
+++ xadres-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadres_console
 {
@@ -10,6 +7,13 @@ namespace xadres_console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.WriteLine();
         }
     }
 }
